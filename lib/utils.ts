788469238862bdd616e815f42ea7d59499113161
tudorcocaina <tudorcocaina@yahoo.com>
2024-06-15 12:10:34 +0300

    fix hours

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,15 +7,12 @@ export function cn(...inputs: ClassValue[]) {
 
 export const ableToVote = (usersToVote: number) => {
   const env = process.env.NODE_ENV;
-  console.log("usersToVote", usersToVote);
-  console.log("env", env);
   const today = new Date();
   if (env === "development") {
-    today.setHours(10, 0, 0, 0);
+    today.setHours(10 + usersToVote, 0, 0, 0);
   } else {
     today.setHours(7 + usersToVote, 0, 0, 0);
   }
-  today.setHours(10 + usersToVote, 0, 0, 0);
   console.log(new Date(), today, new Date() > today);
   return new Date() > today;
 };
