66448d5330266ca663b4d31159d70bdf7454900c
tudorcocaina <tudorcocaina@yahoo.com>
2024-06-15 11:56:59 +0300

    logs

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const ableToVote = (usersToVote: number) => {
+  console.log("usersToVote", usersToVote);
   const today = new Date();
   today.setHours(10 + usersToVote, 0, 0, 0);
+  console.log(new Date(), today, new Date() > today);
   return new Date() > today;
 };
